118eac634f8f02e9e3f8ef1991e31796a1d5210d
Jarrod Savard <Jarrodsavard2x180@gmail.com>
2020-06-27 20:25:01 -0700

    added function 'check' to check if words in WORD_INDEX_CHECK_LIST match wrodList's child LI nodes and added a space bar event listener

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+//Document constants
 const wordListContainer = document.querySelector(".word-list-container");
 const wordList = document.querySelector(".word-list");
 const userInput = document.querySelector(".word-input");
@@ -5,8 +6,12 @@ const correctCount = document.querySelector(".correct-words-count");
 const incorrectCount = document.querySelector(".incorrect-words-count");
 const timeLeft = document.querySelector(".time-left");
 
-//Document constants
+//space bar event listener
+userInput.addEventListener("keyup", (e) => {
+  e.code === "Space" ? (userInput.value = "") : console.log(userInput.value);
+});
 
+//fetching list and creating a index pair of 'words' and 'randomNumber'
 const SHOWN_WORDS = 50;
 let WORD_INDEX_CHECK_LIST = [];
 
@@ -15,9 +20,23 @@ fetch("src/js/words.txt")
   .then((data) => {
     const words = data.split(/\n/);
     for (let i = 0; i < SHOWN_WORDS; i++) {
-      const randomNumber = Math.floor(Math.random() * words.length + 1);
+      const randomNumber = Math.floor(Math.random() * words.length);
       wordList.innerHTML += `<li>${words[randomNumber]}</li>`;
-      WORD_INDEX_CHECK_LIST.push(randomNumber);
+      WORD_INDEX_CHECK_LIST.push(randomNumber + 1);
     }
-    console.log(WORD_INDEX_CHECK_LIST);
+    console.log(wordList.childNodes[0]);
+    console.log(WORD_INDEX_CHECK_LIST[0]);
   });
+
+const check = () => {
+  for (let i = 0; i < SHOWN_WORDS; i++) {
+    if (wordList.childNodes[i] === WORD_INDEX_CHECK_LIST[i]) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  }
+  console.log(wordList.childNodes, WORD_INDEX_CHECK_LIST);
+};
+
+check();
