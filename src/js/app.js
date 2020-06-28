a16659e29c516d430732f5b2eaaef7574562575b
Jarrod Savard <Jarrodsavard2x180@gmail.com>
2020-06-27 19:07:20 -0700

    added WORD_INDEX_CHECK_LIST and modified randomNumber to track the index of the words within the wordList.innerHTML

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-const wordListContainer = document.querySelector('.word-list-container')
+const wordListContainer = document.querySelector(".word-list-container");
 const wordList = document.querySelector(".word-list");
 const userInput = document.querySelector(".word-input");
 const correctCount = document.querySelector(".correct-words-count");
@@ -8,14 +8,16 @@ const timeLeft = document.querySelector(".time-left");
 //Document constants
 
 const SHOWN_WORDS = 50;
+let WORD_INDEX_CHECK_LIST = [];
 
 fetch("src/js/words.txt")
   .then((response) => response.text())
   .then((data) => {
     const words = data.split(/\n/);
     for (let i = 0; i < SHOWN_WORDS; i++) {
-      const randomNumber = Math.floor(Math.random() * words.length);
+      const randomNumber = Math.floor(Math.random() * words.length + 1);
       wordList.innerHTML += `<li>${words[randomNumber]}</li>`;
+      WORD_INDEX_CHECK_LIST.push(randomNumber);
     }
+    console.log(WORD_INDEX_CHECK_LIST);
   });
-
